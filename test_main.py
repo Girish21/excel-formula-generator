c50a34a72831960eef1907c03e367683422f6714
Girish <girishkumarv21@gmail.com>
2020-01-08 21:02:01 +0530

    init core test

---
+++ test_main.py
@@ -0,0 +1,18 @@
+import unittest
+import main
+from openpyxl import load_workbook, Workbook
+
+
+class TestMainFunctionality(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_result(self):
+        pass
+
+    def test_find_name(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
